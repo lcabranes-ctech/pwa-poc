c8c101a8e928b9cd8e9fa5a3cb2d47b99b18c0f2
Leandro Cabranes <leandro.cabranes@c-tech.com.ar>
2022-08-25 12:30:06 -0300

    ajustes cambio api

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,17 +1,19 @@
 import { LightningElement } from 'lwc';
 
-const CONTACTS_URL = 'api/contacts';
-
 export default class App extends LightningElement {
-
     contacts = [];
 
     connectedCallback() {
-        fetch(CONTACTS_URL).then(response => {
-            return response.json();
-        }).then( contacts => {
-            this.contacts = contacts;
-        });
+        fetch('https://reqres.in/api/users')
+            .then((response) => {
+                return response.json();
+            })
+            .then((contacts) => {
+                this.contacts = contacts.data;
+            });
     }
 
+    handleClick() {
+        console.log('hola');
+    }
 }
